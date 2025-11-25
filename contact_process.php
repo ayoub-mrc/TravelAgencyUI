f83b01195e19ba27fcb25bb804d3a8c80a1b7ccb
ayoub-mrc <fellahayoubb146@example.com>
2025-11-25 18:23:52 +0100

    Add send email

--- contact_process.php
+++ contact_process.php
@@ -6,26 +6,40 @@ require 'PHPMailer-master/src/Exception.php';
 require 'PHPMailer-master/src/PHPMailer.php';
 require 'PHPMailer-master/src/SMTP.php';
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $mail = new PHPMailer(true);
+$mail->CharSet = 'UTF-8';
+$mail->Encoding = 'base64';
 
 try {
     // SMTP settings
+    $mail->SMTPDebug = 2;
+    $mail->Debugoutput = 'html';
+
     $mail->isSMTP();
     $mail->Host = "smtp.gmail.com";
     $mail->SMTPAuth = true;
-    $mail->Username = "ayoufellahb14n19@gmail.com";   // بريدك
-    $mail->Password = "amak xbtw oaem oalv";           // كلمة مرور التطبيقات
+    $mail->Username = "ayoubfellahb14n19@gmail.com";   // بريدك
+    $mail->Password = "mjgoyrkmluzluopa";           // كلمة مرور التطبيقات
     $mail->SMTPSecure = "tls";
     $mail->Port = 587;
 
     // Sender & Recipient
-    $mail->setFrom($_POST['email'], $_POST['name']);
-    $mail->addAddress("ayoufellahb14n19@gmail.com");
+    $mail->setFrom("ayoubfellahb14n19@gmail.com", "ASAFAR Contact");
+
+    // Reply-To 
+    $mail->addReplyTo($_POST['email'], $_POST['name']);
+
+    // Recipient 
+    $mail->addAddress("ayoubfellahb14n19@gmail.com");
+
 
     // Content
     $mail->isHTML(true);
     $mail->Subject = $_POST['subject'];
-    $mail->Body = nl2br($_POST['message']);
+    $mail->Body = nl2br($_POST['message']) ?? 'Message from contact form';
 
     $mail->send();
     echo "success";
